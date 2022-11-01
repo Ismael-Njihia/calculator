74d8a352610af8928851670193f3e2f7dba0df94
EngIshmael <engIshmaelnjihia@gmail.com>
2022-11-01 19:38:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,226 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.example.calculator.databinding.ActivityMainBinding;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    public EditText e1, e2;
+    TextView t1;
+    int num1, num2;
+
+    public boolean getNumbers(){
+        e1 = (EditText) findViewById(R.id.num1);
+        e2 = (EditText) findViewById(R.id.num2);
+
+        t1 = (TextView) findViewById(R.id.result);
+
+        String s1 = e1.getText().toString();
+        String s2 = e2.getText().toString();
+
+        if(s1.equals("Please enter a value 1") && s2.equals(null)){
+            String result = "please enter a value 2";
+            e2.setText(result);
+            return false;
+        }
+        if(s1.equals(null) && s2.equals("Please enter value 2"))
+        {
+            String result = "Please enter value 1";
+            e1.setText(result);
+            return false;
+        }
+        if(s1.equals("Please enter value 1") || s2.equals("Please enter value 2"))
+        {
+            return false;
+        }
+
+        if((!s1.equals(null) && s2.equals(null))|| (!s1.equals("") && s2.equals("")) ){
+
+            String result = "Please enter value 2";
+
+            e2.setText(result);
+            return false;
+        }
+        if((s1.equals(null) && !s2.equals(null))|| (s1.equals("") && !s2.equals("")) ){
+            //checkAndClear();
+            String result = "Please enter value 1";
+            e1.setText(result);
+            return false;
+        }
+        if((s1.equals(null) && s2.equals(null))|| (s1.equals("") && s2.equals("")) ){
+            //checkAndClear();
+            String result1 = "Please enter value 1";
+            e1.setText(result1);
+            String result2 = "Please enter value 2";
+            e2.setText(result2);
+            return false;
+        }
+
+        else {
+            // converting string to int.
+            num1 = Integer.parseInt(s1);
+
+            // converting string to int.
+            num2 = Integer.parseInt(s2);
+
+
+        }
+
+        return true;
+    }
+
+    public void doSum(View v) {
+
+        // get the input numbers
+        if (getNumbers()) {
+            int sum = num1 + num2;
+            t1.setText(Integer.toString(sum));
+        }
+        else
+        {
+            t1.setText("Error Please enter Required Values");
+        }
+
+    }
+    public void clearTextNum1(View v) {
+
+        // get the input numbers
+        e1.getText().clear();
+    }
+    public void clearTextNum2(View v) {
+
+        // get the input numbers
+        e2.getText().clear();
+    }
+    public void doPow(View v) {
+
+        //checkAndClear();
+        // get the input numbers
+        if (getNumbers()) {
+            double sum = Math.pow(num1, num2);
+            t1.setText(Double.toString(sum));
+        }
+        else
+        {
+            t1.setText("Error Please enter Required Values");
+        }
+    }
+
+    // a public method to perform subtraction
+    public void doSub(View v) {
+        //checkAndClear();
+        // get the input numbers
+        if (getNumbers()) {
+            int sum = num1 - num2;
+            t1.setText(Integer.toString(sum));
+        }
+        else
+        {
+            t1.setText("Error Please enter Required Values");
+        }
+    }
+
+    // a public method to perform multiplication
+    public void doMul(View v) {
+        //checkAndClear();
+        // get the input numbers
+        if (getNumbers()) {
+            int sum = num1 * num2;
+            t1.setText(Integer.toString(sum));
+        }
+        else
+        {
+            t1.setText("Error Please enter Required Values");
+        }
+    }
+
+    // a public method to perform Division
+    public void doDiv(View v) {
+        //checkAndClear();
+        // get the input numbers
+        if (getNumbers()) {
+
+            // displaying the text in text view assigned as t1
+            double sum = num1 / (num2 * 1.0);
+            t1.setText(Double.toString(sum));
+        }
+        else
+        {
+            t1.setText("Error Please enter Required Values");
+        }
+    }
+
+    // a public method to perform modulus function
+    public void doMod(View v) {
+        //checkAndClear();
+        // get the input numbers
+        if (getNumbers()) {
+            double sum = num1 % num2;
+            t1.setText(Double.toString(sum));
+        }
+        else
+        {
+            t1.setText("Error Please enter Required Values");
+        }
+    }
+
+
+
+}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        e1 = (EditText) findViewById(R.id.num1);
+        // defining the edit text 2 to e2
+        e2 = (EditText) findViewById(R.id.num2);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
+}
